b4c2e3f9daab3365eebea657b0bf6fc8cb245f57
Utkarsh Vijay <utkarshvijay99@gmail.com>
2021-08-05 01:17:34 +0530

    some changes in chatbox

--- src/components/chatbox/Chatbox.jsx
+++ src/components/chatbox/Chatbox.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 import "./Chatbox.css";
+import {
+  EditLocationRounded,
+  HelpRounded,
+  Notifications,
+  PeopleAltRounded,
+  SearchRounded,
+  SendRounded,
+} from "@material-ui/icons";
 function Chatbox() {
   return (
     <div className="chatbox">
-      <h1>Chat box here</h1>
+      <header className="chatbox__header">
+        <div className="chatbox__header__left">
+          <h3>
+            <span className="chatbox__header__hash">#</span>
+            main
+          </h3>
+        </div>
+
+        <div className="chatHeader__right">
+          <Notifications />
+          <EditLocationRounded />
+          <PeopleAltRounded />
+
+          <div className="chatHeader__search">
+            <input placeholder="Search" type="text" />
+            <SearchRounded />
+          </div>
+
+          <SendRounded />
+          <HelpRounded />
+        </div>
+      </header>
     </div>
   );
 }
