daf0e4ce38a8210f54f8151f649026c810e7773e
sanctum007 <pushpendrayadav1057@gmail.com>
2021-08-05 22:22:51 +0530

    started working on channels

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ChannelNavbar from "../ChannelNavbar/ChannelNavbar";
 import ServerNavbar from "../ServerNavbar/ServerNavbar";
 import "./Sidebar.css";
 function Sidebar() {
   return (
     <div className="sidebar">
       <ServerNavbar />
+      <ChannelNavbar />
     </div>
   );
 }
