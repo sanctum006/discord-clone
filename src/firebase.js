2dfdd7923ed73c9fb42270e0120461afeb485e35
sanctum006 <74344331+sanctum006@users.noreply.github.com>
2022-11-08 12:17:21 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCoWKNlfqmYyGqzu8NjtiRoiCIi_uUr_8U",
-  authDomain: "discord-clone-101.firebaseapp.com",
-  projectId: "discord-clone-101",
-  storageBucket: "discord-clone-101.appspot.com",
-  messagingSenderId: "72844495235",
-  appId: "1:72844495235:web:859e169faf0985382b0eb6",
-};
+const firebaseConfig = {};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
